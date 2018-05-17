81d2f3ea66ef9d8f635c1a67774114280bfc5243
James Knippel <jamesknippel@gmail.com>
2018-05-16 21:17:45 -0700

    auth import refactor

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Router } from '@angular/router';
-import * as firebase from 'firebase/app';
+import firebase from '@firebase/app';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 
